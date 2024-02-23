ba8d346441d272b5be165ecb5d35c24845c7d593
Ahmed Najeh <79747792+M4xIq@users.noreply.github.com>
2024-02-23 20:10:53 +0300

    Update CSRF_PRO.php

--- CSRF_PRO.php
+++ CSRF_PRO.php
@@ -3,7 +3,7 @@
 <head>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>Split Page</title>
+<title>CSRF_PRO</title>
 <style>
     body {
         font-family: Arial, sans-serif;
